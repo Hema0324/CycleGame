2b8b4e0b59238318097e4ecf599623c13db17a96
Hema0324 <63283241+Hema0324@users.noreply.github.com>
2024-07-15 16:30:04 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,12 +2,20 @@
 var path,pathImg;
 var player1,player2,player3;
 var mainPlayerImg,mainCyclist;
+var ObstacleImg,Obstacle,ObstacleImg2,ObstacleImg3,ObstacleImg4,ObstacleImg5,ObstacleImg6;
 
 
 function preload()
 {
  pathImg = loadImage("Road.png")
  mainPlayerImg = loadAnimation("mainPlayer1.png","mainPlayer2.png")
+ ObstacleImg= loadImage("obstacle1.png")
+ ObstacleImg2= loadImage("obstacle2.png")
+ ObstacleImg3= loadImage("obstacle3.png")
+// ObstacleImg4= loadImage("opponent1.png")
+ //ObstacleImg5= loadImage("opponent5.png")
+ //ObstacleImg6= loadImage("opponent7.png")
+ 
 
 }
 
@@ -23,7 +31,12 @@ function setup()
   mainCyclist = createSprite(70,150)
   mainCyclist.addAnimation("Running",mainPlayerImg)
   mainCyclist.scale = 0.08
+ 
+   
+  
 
+
+  
 
  
 }
@@ -35,6 +48,25 @@ function draw()
   {
     path.x = path.width/2
   }
+
+
+  
+ 
+ 
+
+  if(frameCount%200==0)
+  {
+     Obstacle= createSprite(Math.round(random(800,1600)),80)
+     Obstacle.scale=0.08;
+     Obstacle.velocityX=-4 
+     var count=Math.round(random(1,6));
+  
+     Obstacle.addImage(ObstacleImg);
+            
+      
+} 
+  
+
   drawSprites()
 
 }
